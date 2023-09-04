cae167adf20d13f0e3b676e473d134c665ffc2c3
Aleksandr Kurnosov <q-saku@ya.ru>
2023-09-04 19:41:46 +0300

    fix

--- main.py
+++ main.py
@@ -103,7 +103,9 @@ async def callback_handler(callback_query: types.CallbackQuery, state: FSMContex
     args = callback_query.data.split("_", 1)
     logger.info(args)
     if args[0] == 'set':
+        logger.info(state)
         async with state.proxy() as d:
+            logger.warning('HERE I AM')
             d['model'] = args[1]
         callback_query.answer(f'Выставлена модель {args[1]}')
 
